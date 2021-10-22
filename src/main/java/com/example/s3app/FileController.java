765672527db53b482cb1726e479fd7a50e2f6043
Hubert Rosinski <hrosinski@pgs-soft.com>
2021-10-22 10:52:30 +0200

    init

---
+++ src/main/java/com/example/s3app/FileController.java
@@ -0,0 +1,28 @@
+package com.example.s3app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/s3")
+public class FileController {
+
+  @Autowired FileService fileServiceNew;
+
+  @GetMapping("/{fileName}")
+  public ResponseEntity<String> findByName(@PathVariable("fileName") String fileName) {
+    return new ResponseEntity<>(fileServiceNew.findByName(fileName), HttpStatus.OK);
+  }
+
+  @PostMapping("/upload")
+  public ResponseEntity<String> saveFile(@RequestParam("extension") String extension) {
+    return new ResponseEntity<>(fileServiceNew.save(extension), HttpStatus.OK);
+  }
+}
---
+++ src/main/java/com/example/s3app/S3AppApplication.java
@@ -0,0 +1,12 @@
+package com.example.s3app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3AppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(S3AppApplication.class, args);
+  }
+}
